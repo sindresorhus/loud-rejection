2a8095f43af41a2d1a1fbe5af7423cf13f68dc37
Linus Unnebäck <linus@folkdatorn.se>
2015-10-19 22:58:34 +0200

    wip

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 'use strict';
-module.exports = function () {
-	process.on('unhandledRejection', function (err) {
-		if (err instanceof Error) {
-			console.error(err.stack);
-		} else if (err) {
-			console.error('Promise rejected with value:', err);
-		} else {
-			console.error('Promise rejected no value');
-		}
+var installed = false;
 
-		process.exit(1);
+function outputRejectedMessage(err) {
+	if (err instanceof Error) {
+		console.error(err.stack);
+	} else if (err) {
+		console.error('Promise rejected with value:', err);
+	} else {
+		console.error('Promise rejected no value');
+	}
+}
+
+module.exports = function () {
+	var unhandledRejections = [];
+
+	if (installed) {
+		console.trace('WARN: loud rejection called more than once');
+		return;
+	}
+
+	installed = true;
+
+	process.on('unhandledRejection', function (reason, p) {
+		unhandledRejections.push({reason: reason, p: p});
+	});
+
+	process.on('rejectionHandled', function (p) {
+		var index = unhandledRejections.reduce(function (result, item, idx) {
+			return (item.p === p ? idx : result);
+		}, -1);
+		unhandledRejections.splice(index, 1);
+	});
+
+	process.on('exit', function () {
+		if (unhandledRejections.length > 0) {
+			unhandledRejections.forEach(function (x) {
+				outputRejectedMessage(x.reason);
+			});
+			process.exitCode = 1;
+		}
 	});
 };
--- test.js
+++ test.js
@@ -1,11 +1,59 @@
-/* eslint-disable no-new */
 'use strict';
-var fn = require('./');
-
+const fn = require('./');
+const test = require('ava');
 fn();
 
-new Promise(function (resolve, reject) {
-	setImmediate(function () {
-		reject(new Error('unicorn'));
+function fire() {
+	return new Promise(function (resolve, reject) {
+		setImmediate(function () {
+			reject(new Error('unicorn'));
+		});
 	});
+}
+
+function testExit(onConsole) {
+	const _error = console.error;
+
+	console.error = onConsole;
+	process.emit('exit');
+	console.error = _error;
+}
+
+test.serial('Never handle rejection', t => {
+	t.plan(2);
+
+	const promise = fire();
+
+	setTimeout(() => {
+		testExit(msg => {
+			t.regexTest(/unicorn/, msg);
+		});
+
+		// Clean up
+		promise.catch(() => {});
+
+		setTimeout(() => {
+			t.pass('Process is still alive');
+		}, 10);
+	}, 10);
+});
+
+test.serial('Handle rejection later', t => {
+	t.plan(2);
+
+	const promise = fire();
+
+	setTimeout(() => {
+		promise.catch(function (err) {
+			t.is(err.message, 'unicorn');
+		});
+
+		testExit(() => {
+			t.fail();
+		});
+
+		setTimeout(() => {
+			t.pass('Process is still alive');
+		}, 10);
+	}, 10);
 });
