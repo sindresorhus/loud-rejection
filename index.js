9ff9d680321ee7ed892db19b352d6e9d54eb06b6
Linus Unnebäck <linus@folkdatorn.se>
2015-10-19 23:49:38 +0200

    use weak for supperior handling

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 'use strict';
-module.exports = function () {
-	process.on('unhandledRejection', function (err) {
-		if (err instanceof Error) {
-			console.error(err.stack);
-		} else if (err) {
-			console.error('Promise rejected with value:', err);
-		} else {
-			console.error('Promise rejected no value');
-		}
+var weak = require('weak');
+var installed = false;
 
-		process.exit(1);
+module.exports = function () {
+	if (installed) {
+		console.trace('WARN: loud rejection called more than once');
+		return;
+	}
+
+	installed = true;
+
+	var error = new Set();
+	var store = new WeakMap();
+
+	function blowUp(reason) {
+		return function () {
+			error.delete(reason);
+			throw reason;
+		};
+	}
+
+	process.on('unhandledRejection', function (reason, p) {
+		store.set(p, {ref: weak(p, blowUp(reason)), reason: reason});
+		error.add(reason);
+	});
+
+	process.on('rejectionHandled', function (p) {
+		weak.removeCallbacks(store.get(p).ref);
+		error.delete(store.get(p).reason);
+	});
+
+	process.on('exit', function () {
+		if (error.size) {
+			throw error.values().next().value;
+		}
 	});
 };
