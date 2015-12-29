2996e4226b84d9ce8afad0b7d5a0a96ca2d5a6da
Tim De Pauw <hi@tmdpw.eu>
2015-12-29 15:42:08 +0100

    Close #12 PR: Refactoring Array#reduce into Array#findIndex.

--- api.js
+++ api.js
@@ -2,6 +2,16 @@
 
 // WARNING: This undocumented API is subject to change.
 
+// TODO Move ponyfill to separate module.
+var findIndex;
+if (Array.prototype.findIndex) {
+	findIndex = function (arr, callback, thisArg) {
+		return arr.findIndex(callback, thisArg);
+	};
+} else {
+	findIndex = require('array-findindex');
+}
+
 module.exports = function (process) {
 	var unhandledRejections = [];
 
@@ -10,9 +20,9 @@ module.exports = function (process) {
 	});
 
 	process.on('rejectionHandled', function (p) {
-		var index = unhandledRejections.reduce(function (result, item, idx) {
-			return (item.promise === p ? idx : result);
-		}, -1);
+		var index = findIndex(unhandledRejections, function (item) {
+			return (item.promise === p);
+		});
 
 		unhandledRejections.splice(index, 1);
 	});
